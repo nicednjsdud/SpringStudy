c025b3c09a3b644720c70a6eead216775290f59a
JeongWonYoung <nicednjsdud@naver.com>
2022-08-13 00:49:58 +0900

    Test AllbeanTEst

--- hellospring/src/main/java/kr/co/hellospring/discount/FixDiscountPolicy.java
+++ hellospring/src/main/java/kr/co/hellospring/discount/FixDiscountPolicy.java
@@ -2,7 +2,9 @@ package kr.co.hellospring.discount;
 
 import kr.co.hellospring.member.Grade;
 import kr.co.hellospring.member.Member;
+import org.springframework.stereotype.Component;
 
+@Component
 public class FixDiscountPolicy implements DiscountPolicy{
 
     private int discountFixAmount = 1000; // 1000원 할인
