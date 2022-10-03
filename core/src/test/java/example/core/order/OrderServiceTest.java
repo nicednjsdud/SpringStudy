0174034f39501d1d30a90f791265145a7a59eaee
JeongWonYoung <nicednjsdud@naver.com>
2022-10-04 02:11:26 +0900

    Feat AppConfig.java
    app config

--- core/src/test/java/example/core/order/OrderServiceTest.java
+++ core/src/test/java/example/core/order/OrderServiceTest.java
@@ -1,16 +1,25 @@
 package example.core.order;
 
+import example.core.AppConfig;
 import example.core.member.Grade;
 import example.core.member.Member;
 import example.core.member.MemberService;
 import example.core.member.MemberServiceImpl;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class OrderServiceTest {
 
-    MemberService memberService = new MemberServiceImpl();
-    OrderService orderService = new OrderServiceImpl();
+    MemberService memberService;
+    OrderService orderService;
+
+    @BeforeEach
+    public void beforeEach(){
+        AppConfig appConfig = new AppConfig();
+        memberService = appConfig.memberService();
+        orderService = appConfig.orderService();
+    }
 
     @Test
     void createOrder(){
