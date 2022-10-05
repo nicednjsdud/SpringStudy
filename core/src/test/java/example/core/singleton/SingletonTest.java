fb3e7b1b01c13a83d53828dde8fd7aa0ec4824f3
JeongWonYoung <nicednjsdud@naver.com>
2022-10-06 01:07:50 +0900

    Test SingleTonTest
    about pure container

---
+++ core/src/test/java/example/core/singleton/SingletonTest.java
@@ -0,0 +1,28 @@
+package example.core.singleton;
+
+import example.core.AppConfig;
+import example.core.member.MemberService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class SingletonTest {
+
+    @Test
+    @DisplayName("스프링 없는 순수한 DI 컨테이너")
+    void pureContainer(){
+        AppConfig appConfig = new AppConfig();
+        // 1. 조회
+        MemberService memberService1 = appConfig.memberService();
+
+        // 2. 조회
+        MemberService memberService2 = appConfig.memberService();
+
+        System.out.println("memberService1 = " + memberService1);
+        System.out.println("memberService2 = " + memberService2);
+
+        assertThat(memberService1).isNotSameAs(memberService2);
+    }
+}
