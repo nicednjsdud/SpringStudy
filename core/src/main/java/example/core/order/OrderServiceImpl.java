806a69a44198dd84f46038e61c0330740d7cc1e3
JeongWonYoung <nicednjsdud@naver.com>
2022-10-06 15:03:03 +0900

    Change bean config
    change config.class -> AutoConfig.class

--- core/src/main/java/example/core/order/OrderServiceImpl.java
+++ core/src/main/java/example/core/order/OrderServiceImpl.java
@@ -6,12 +6,16 @@ import example.core.discount.RateDiscountPolicy;
 import example.core.member.Member;
 import example.core.member.MemberRepository;
 import example.core.member.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
 public class OrderServiceImpl implements OrderService {
 
     private MemberRepository memberRepository;
     private DiscountPolicy discountPolicy;
 
+    @Autowired
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
