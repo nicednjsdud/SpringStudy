5eaf2b278fd1ab63a4f0aeb7ed2980a02c0f0577
JeongWonYoung <nicednjsdud@naver.com>
2022-10-06 14:01:10 +0900

    Test StatefulServiceTest
    about singleton status

---
+++ core/src/test/java/example/core/singleton/StatefulServiceTest.java
@@ -0,0 +1,40 @@
+package example.core.singleton;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class StatefulServiceTest {
+
+    @Test
+    void statefulServiceSingleton(){
+        ApplicationContext ac = new AnnotationConfigApplicationContext(TestConfig.class);
+        StatefulService statefulService1 = ac.getBean(StatefulService.class);
+        StatefulService statefulService2 = ac.getBean(StatefulService.class);
+
+        int userA = statefulService1.order("userA", 10000);
+        int userB = statefulService2.order("userB", 20000);
+
+//        int price = statefulService1.getPrice();
+        System.out.println("userA = " + userA);
+        System.out.println("userB = " + userB);
+
+//        assertThat(userB).isEqualTo(20000);
+    }
+
+    @Configuration
+    static class TestConfig{
+
+        @Bean
+        public StatefulService statefulService(){
+            return new StatefulService();
+        }
+    }
+
+}
