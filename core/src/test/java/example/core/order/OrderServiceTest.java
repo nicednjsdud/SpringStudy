104e4aa30b9acc516abbec130cf80cefb4e909a2
JeongWonYoung <nicednjsdud@naver.com>
2022-10-07 12:41:35 +0900

    Test AutowiredTest
    about nullable , optional

--- core/src/test/java/example/core/order/OrderServiceTest.java
+++ core/src/test/java/example/core/order/OrderServiceTest.java
@@ -31,4 +31,11 @@ public class OrderServiceTest {
         Assertions.assertThat(order.getDiscountPrice()).isEqualTo(1000);
 
     }
+
+//    @Test
+//    void fieldInjectionTest(){
+//        OrderServiceImpl orderService = new OrderServiceImpl();
+//        orderService.createOrder(1L, "itemA", 10000);
+//
+//    }
 }
