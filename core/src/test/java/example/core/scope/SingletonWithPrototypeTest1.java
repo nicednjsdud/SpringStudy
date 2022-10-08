6ad910106844a049de002c21f4ae4a5a075380f3
JeongWonYoung <nicednjsdud@naver.com>
2022-10-08 16:27:12 +0900

    Test singletonWithPrototypeTest1

--- core/src/test/java/example/core/scope/SingletonWithPrototypeTest1.java
+++ core/src/test/java/example/core/scope/SingletonWithPrototypeTest1.java
@@ -10,33 +10,40 @@ import javax.annotation.PreDestroy;
 
 import static org.assertj.core.api.Assertions.*;
 
-public class PrototypeTest {
+public class SingletonWithPrototypeTest1 {
 
     @Test
-    void prototypeBeanFind() {
+    void prototypeFind() {
         AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(PrototypeBean.class);
-        System.out.println("find prototypeBean1");
         PrototypeBean prototypeBean1 = ac.getBean(PrototypeBean.class);
-        System.out.println("find prototypeBean2");
+        prototypeBean1.addCount();
+        assertThat(prototypeBean1.getCount()).isEqualTo(1);
+
         PrototypeBean prototypeBean2 = ac.getBean(PrototypeBean.class);
-        System.out.println("prototypeBean1 = " + prototypeBean1);
-        System.out.println("prototypeBean2 = " + prototypeBean2);
-
-        assertThat(prototypeBean1).isNotSameAs(prototypeBean2);
-
-        ac.close();
+        prototypeBean2.addCount();
+        assertThat(prototypeBean2.getCount()).isEqualTo(1);
     }
 
     @Scope("prototype")
     static class PrototypeBean {
+        private int count = 0;
+
+        public void addCount() {
+            count++;
+        }
+
+        public int getCount() {
+            return count;
+        }
+
         @PostConstruct
         public void init() {
-            System.out.println("SingletonBean.init");
+            System.out.println("PrototypeBean.init " + this);
         }
 
         @PreDestroy
         public void destroy() {
-            System.out.println("SingletonBean.destroy");
+            System.out.println("PrototypeBean.destroy");
         }
     }
 }
