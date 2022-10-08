0e40a7bc5138ef9a1017cf0701a5a7ff815e3c7f
JeongWonYoung <nicednjsdud@naver.com>
2022-10-08 15:11:34 +0900

    Fix NetworkClient
    add initMethod , destroymethod

--- core/src/test/java/example/core/lifecycle/NetworkClient.java
+++ core/src/test/java/example/core/lifecycle/NetworkClient.java
@@ -3,7 +3,7 @@ package example.core.lifecycle;
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
 
-public class NetworkClient implements InitializingBean, DisposableBean {
+public class NetworkClient {
 
     private String url;
 
@@ -29,14 +29,16 @@ public class NetworkClient implements InitializingBean, DisposableBean {
         System.out.println("close = " + url);
     }
 
-    @Override
-    public void afterPropertiesSet() throws Exception {
+
+    public void init() throws Exception {
+        System.out.println("NetworkClient.init");
         connect();
         call("초기화 연결 메세지");
     }
 
-    @Override
-    public void destroy() throws Exception {
+
+    public void close() throws Exception {
+        System.out.println("NetworkClient.close");
         disconnect();
     }
 }
