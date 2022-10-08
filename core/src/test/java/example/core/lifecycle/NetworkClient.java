3d4998d5bb4dbc09054db073673eea99e0a37c42
JeongWonYoung <nicednjsdud@naver.com>
2022-10-08 15:15:50 +0900

    Feat annotation @PostConstruct , @PreDestory

--- core/src/test/java/example/core/lifecycle/NetworkClient.java
+++ core/src/test/java/example/core/lifecycle/NetworkClient.java
@@ -3,6 +3,9 @@ package example.core.lifecycle;
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 public class NetworkClient {
 
     private String url;
@@ -21,7 +24,7 @@ public class NetworkClient {
     }
 
     public void call(String message) {
-        System.out.println("call= " + url + "message = " + message);
+        System.out.println("call= " + url + " message = " + message);
     }
 
     // 서비스 종료시 호출
@@ -29,14 +32,14 @@ public class NetworkClient {
         System.out.println("close = " + url);
     }
 
-
+    @PostConstruct
     public void init() throws Exception {
         System.out.println("NetworkClient.init");
         connect();
         call("초기화 연결 메세지");
     }
 
-
+    @PreDestroy
     public void close() throws Exception {
         System.out.println("NetworkClient.close");
         disconnect();
