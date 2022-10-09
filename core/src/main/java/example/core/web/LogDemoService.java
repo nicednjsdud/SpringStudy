218f5e673f5f8380a5b1e15484e48130eb6c149a
JeongWonYoung <nicednjsdud@naver.com>
2022-10-09 14:38:38 +0900

    Feat MyLogger annotation
    add proxy mode

--- core/src/main/java/example/core/web/LogDemoService.java
+++ core/src/main/java/example/core/web/LogDemoService.java
@@ -2,17 +2,15 @@ package example.core.web;
 
 import example.core.common.MyLogger;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class LogDemoService {
 
-    private final ObjectProvider<MyLogger> myLoggerProvider;
+    private final MyLogger myLogger;
 
     public void logic(String id) {
-        MyLogger myLogger = myLoggerProvider.getObject();
-        myLogger.log("service id= " + id);
+        myLogger.log("service = " + id);
     }
 }
