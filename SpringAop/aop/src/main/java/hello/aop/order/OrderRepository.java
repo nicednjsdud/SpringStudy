c547a5209f30f8671039e11e87e6697e15ffdeef
JeongWonYoung <nicednjsdud@naver.com>
2025-09-09 12:32:56 +0900

    add aop depengency

---
+++ SpringAop/aop/src/main/java/hello/aop/order/OrderRepository.java
@@ -0,0 +1,18 @@
+package hello.aop.order;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Repository;
+
+@Slf4j
+@Repository
+public class OrderRepository {
+
+    public String save(String itemId) {
+        log.info("[OrderRepository] execute");
+        // save logic
+        if (itemId.equals("ex")) {
+            throw new IllegalStateException("예외 발생!");
+        }
+        return "ok";
+    }
+}
