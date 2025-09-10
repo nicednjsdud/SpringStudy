0122de442cad54ae926c9c393d0bffab8f8c76c4
JeongWonYoung <nicednjsdud@naver.com>
2025-09-10 13:28:26 +0900

    bean 등록

---
+++ SpringAop/aop/src/test/java/hello/aop/pointcut/BeanTest.java
@@ -0,0 +1,42 @@
+package hello.aop.pointcut;
+
+
+import hello.aop.order.OrderService;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@Slf4j
+@SpringBootTest
+@Import(BeanTest.BeanAspect.class)
+public class BeanTest {
+
+    @Autowired
+    OrderService orderService;
+
+    @Test
+    void success() {
+        log.info("orderService Proxy={}", orderService.getClass());
+        orderService.orderItem("itemA");
+    }
+
+    @Aspect
+    static class BeanAspect {
+        @Around("bean(orderService) || bean(*Repository)")
+        public Object doLog(ProceedingJoinPoint joinPoint) throws Throwable {
+            log.info("[bean] {}", joinPoint.getSignature());
+            return joinPoint.proceed();
+        }
+
+    }
+
+    // [bean] void hello.aop.order.OrderService.orderItem(String)
+    // [OrderService] execute
+    // [bean] String hello.aop.order.OrderRepository.save(String)
+    // [OrderRepository] execute
+}
