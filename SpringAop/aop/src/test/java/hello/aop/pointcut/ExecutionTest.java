18b832e5071938fa55c0cbb7d4989af5739cd2d5
JeongWonYoung <nicednjsdud@naver.com>
2025-09-10 12:39:19 +0900

    execution 사용법 공부 - 1

--- SpringAop/aop/src/test/java/hello/aop/pointcut/ExecutionTest.java
+++ SpringAop/aop/src/test/java/hello/aop/pointcut/ExecutionTest.java
@@ -24,4 +24,159 @@ public class ExecutionTest {
         // public java.lang.String hello.aop.member.MemberServiceImpl.hello(java.lang.String)
         log.info("helloMethod={}", helloMethod);
     }
+
+    @Test
+    void exactMatch() {
+        // public java.lang.String hello.aop.member.MemberServiceImpl.hello(java.lang.String)
+        pointcut.setExpression("execution(public String hello.aop.member.MemberServiceImpl.hello(String))");
+        assert pointcut.matches(helloMethod, MemberServiceImpl.class);
+
+        // 매칭 조건
+        // 접근제어자? public
+        // 반환타입    String
+        // 선언타입?   hello.aop.member.MemberServiceImpl
+        // 메서드명    hello
+        // 파라미터    String
+        // 예외      생략
+    }
+
+    @Test
+    void allMatch() {
+        pointcut.setExpression("execution(* *(..))");
+        assert pointcut.matches(helloMethod, MemberServiceImpl.class);
+
+        // 매칭 조건
+        // 접근제어자? 생략
+        // 반환타입    *
+        // 선언타입?   생략
+        // 메서드명    *
+        // 파라미터    (..)
+        // 예외      생략
+    }
+
+    @Test
+    void nameMatch() {
+        pointcut.setExpression("execution(* hello(..))");
+        assert pointcut.matches(helloMethod, MemberServiceImpl.class);
+
+        // 매칭 조건
+        // 접근제어자? 생략
+        // 반환타입    *
+        // 선언타입?   생략
+        // 메서드명    hello
+        // 파라미터    (..)
+        // 예외      생략
+    }
+
+    @Test
+    void nameMatchStar1() {
+        pointcut.setExpression("execution(* hel*(..))");
+        assert pointcut.matches(helloMethod, MemberServiceImpl.class);
+
+        // 매칭 조건
+        // 접근제어자? 생략
+        // 반환타입    *
+        // 선언타입?   생략
+        // 메서드명    hel*
+        // 파라미터    (..)
+        // 예외      생략
+    }
+
+    @Test
+    void nameMatchStar2() {
+        pointcut.setExpression("execution(* *el*(..))");
+        assert pointcut.matches(helloMethod, MemberServiceImpl.class);
+
+        // 매칭 조건
+        // 접근제어자? 생략
+        // 반환타입    *
+        // 선언타입?   생략
+        // 메서드명    *el*
+        // 파라미터    (..)
+        // 예외      생략
+    }
+
+    @Test
+    void nameMatchFalse() {
+        pointcut.setExpression("execution(* nono(..))");
+        assert !pointcut.matches(helloMethod, MemberServiceImpl.class);
+
+        // 매칭 조건
+        // 접근제어자? 생략
+        // 반환타입    *
+        // 선언타입?   생략
+        // 메서드명    nono
+        // 파라미터    (..)
+        // 예외      생략
+    }
+
+    @Test
+    void packageExactMatch1() {
+        pointcut.setExpression("execution(* hello.aop.member.MemberServiceImpl.hello(..))");
+        assert pointcut.matches(helloMethod, MemberServiceImpl.class);
+
+        // 매칭 조건
+        // 접근제어자? 생략
+        // 반환타입    *
+        // 선언타입?   hello.aop.member.MemberServiceImpl
+        // 메서드명    hello
+        // 파라미터    (..)
+        // 예외      생략
+    }
+
+    @Test
+    void packageExactMatch2() {
+        pointcut.setExpression("execution(* hello.aop.member.*.*(..))");
+        assert pointcut.matches(helloMethod, MemberServiceImpl.class);
+
+        // 매칭 조건
+        // 접근제어자? 생략
+        // 반환타입    *
+        // 선언타입?   hello.aop.member.*
+        // 메서드명    *
+        // 파라미터    (..)
+        // 예외      생략
+    }
+
+    @Test
+    void packageExactFalse() {
+        pointcut.setExpression("execution(* hello.aop.*.*(..))");
+        assert !pointcut.matches(helloMethod, MemberServiceImpl.class);
+
+        // 매칭 조건
+        // 접근제어자? 생략
+        // 반환타입    *
+        // 선언타입?   hello.aop.*
+        // 메서드명    *
+        // 파라미터    (..)
+        // 예외      생략
+    }
+
+    @Test
+    void packageMatchSubPackage1() {
+        pointcut.setExpression("execution(* hello.aop.member..*.*(..))");
+        assert pointcut.matches(helloMethod, MemberServiceImpl.class);
+
+        // 매칭 조건
+        // 접근제어자? 생략
+        // 반환타입    *
+        // 선언타입?   hello.aop.member..
+        // 메서드명    *
+        // 파라미터    (..)
+        // 예외      생략
+    }
+
+    @Test
+    void packageMatchSubPackage2() {
+        pointcut.setExpression("execution(* hello.aop..*.*(..))");
+        assert pointcut.matches(helloMethod, MemberServiceImpl.class);
+
+        // 매칭 조건
+        // 접근제어자? 생략
+        // 반환타입    *
+        // 선언타입?   hello.aop..
+        // 메서드명    *
+        // 파라미터    (..)
+        // 예외      생략
+    }
 }
