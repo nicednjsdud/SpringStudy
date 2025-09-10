504bc2c50e667c70ce1201b9270055859483db6f
JeongWonYoung <nicednjsdud@naver.com>
2025-09-10 12:55:34 +0900

    execution 사용법 공부 - 2

--- SpringAop/aop/src/test/java/hello/aop/pointcut/ExecutionTest.java
+++ SpringAop/aop/src/test/java/hello/aop/pointcut/ExecutionTest.java
@@ -179,4 +179,160 @@ public class ExecutionTest {
         // 파라미터    (..)
         // 예외      생략
     }
+
+    @Test
+    void typeExactMatch() {
+        pointcut.setExpression("execution(* hello.aop.member.MemberServiceImpl.*(..))");
+        assert pointcut.matches(helloMethod, MemberServiceImpl.class);
+
+        // 매칭 조건
+        // 접근제어자? 생략
+        // 반환타입    *
+        // 선언타입?   hello.aop.member.MemberServiceImpl
+        // 메서드명    *
+        // 파라미터    (..)
+        // 예외      생략
+    }
+
+    @Test
+    void typeMatchSuperType() {
+        pointcut.setExpression("execution(* hello.aop.member.MemberService.*(..))");
+        assert pointcut.matches(helloMethod, MemberServiceImpl.class);
+
+        // 매칭 조건
+        // 접근제어자? 생략
+        // 반환타입    *
+        // 선언타입?   hello.aop.member.MemberService (MemberServiceImpl의 부모 타입)
+        // 메서드명    *
+        // 파라미터    (..)
+        // 예외      생략
+
+        // 부모 타입을 선언해도 자식 타입인 MemberServiceImpl의 hello 메서드가 매칭된다.
+        // 즉, 선언된 타입에 있는 메서드만 매칭된다.
+        // 부모 타입에 선언된 메서드가 자식 타입에 오버라이딩 되어있지 않으면 매칭되지 않는다.
+    }
+
+    @Test
+    void typeMatchInternal() throws NoSuchMethodException {
+        pointcut.setExpression("execution(* hello.aop.member.MemberService.*(..))");
+        Method internalMethod = MemberServiceImpl.class.getMethod("internal", String.class);
+        assert !pointcut.matches(internalMethod, MemberServiceImpl.class);
+
+        // 매칭 조건
+        // 접근제어자? 생략
+        // 반환타입    *
+        // 선언타입?   hello.aop.member.MemberService (MemberServiceImpl의 부모 타입)
+        // 메서드명    *
+        // 파라미터    (..)
+        // 예외      생략
+
+        // internal 메서드는 MemberService 인터페이스에 선언되어 있지 않기 때문에 매칭되지 않는다.
+    }
+
+    @Test
+    void typeMatchNoSuperTypeMethodFalse() throws NoSuchMethodException {
+        pointcut.setExpression("execution(* hello.aop.member.MemberService.*(..))");
+        Method internalMethod = MemberServiceImpl.class.getMethod("internal", String.class);
+        assert !pointcut.matches(internalMethod, MemberServiceImpl.class);
+
+        // 매칭 조건
+        // 접근제어자? 생략
+        // 반환타입    *
+        // 선언타입?   hello.aop.member.MemberServiceImpl
+        // 메서드명    *
+        // 파라미터    (..)
+        // 예외      생략
+
+        // MemberServiceImpl에 선언된 모든 메서드가 매칭되지 않는다.
+    }
+
+    // String 타입 파라미터 허용
+    @Test
+    void argsMatch() {
+        pointcut.setExpression("execution(* *(String))");
+        assert pointcut.matches(helloMethod, MemberServiceImpl.class);
+
+        // 매칭 조건
+        // 접근제어자? 생략
+        // 반환타입    *
+        // 선언타입?   생략
+        // 메서드명    *
+        // 파라미터    (String)
+        // 예외      생략
+
+        // String 타입의 파라미터 허용
+    }
+
+    // 파라미터 없음
+    @Test
+    void argsMatchNoArgs() {
+        pointcut.setExpression("execution(* *())");
+        assert !pointcut.matches(helloMethod, MemberServiceImpl.class);
+
+        // 매칭 조건
+        // 접근제어자? 생략
+        // 반환타입    *
+        // 선언타입?   생략
+        // 메서드명    *
+        // 파라미터    ()
+        // 예외      생략
+
+        // 파라미터 없음
+    }
+
+    // 정확히 하나의 파라미터, 모든 타입 허용
+    @Test
+    void argsMatchStar() {
+        pointcut.setExpression("execution(* *(*))");
+        assert pointcut.matches(helloMethod, MemberServiceImpl.class);
+
+        // 매칭 조건
+        // 접근제어자? 생략
+        // 반환타입    *
+        // 선언타입?   생략
+        // 메서드명    *
+        // 파라미터    (*)
+        // 예외      생략
+
+        // 정확히 하나의 파라미터, 모든 타입 허용
+    }
+
+    // 숫자 상관없이 모든 파라미터, 모든 타입 허용
+    // (), (Xxx), (Xxx, Yyy) 등 모든 파라미터 허용
+    @Test
+    void argsMatchAll() {
+        pointcut.setExpression("execution(* *(..))");
+        assert pointcut.matches(helloMethod, MemberServiceImpl.class);
+
+        // 매칭 조건
+        // 접근제어자? 생략
+        // 반환타입    *
+        // 선언타입?   생략
+        // 메서드명    *
+        // 파라미터    (..)
+        // 예외      생략
+
+        // 숫자 상관없이 모든 파라미터, 모든 타입 허용
+        // (), (Xxx), (Xxx, Yyy) 등 모든 파라미터 허용
+    }
+
+    // String 타입으로 시작, 숫자 상관없이 모든 파라미터, 모든 타입 허용
+    @Test
+    void argsMatchComplex() {
+        pointcut.setExpression("execution(* *(String, ..))");
+        assert pointcut.matches(helloMethod, MemberServiceImpl.class);
+
+        // 매칭 조건
+        // 접근제어자? 생략
+        // 반환타입    *
+        // 선언타입?   생략
+        // 메서드명    *
+        // 파라미터    (String, ..)
+        // 예외      생략
+
+        // String 타입으로 시작, 숫자 상관없이 모든 파라미터, 모든 타입 허용
+        // (String, Xxx), (String, Xxx, Yyy) 등 모든 파라미터 허용
+    }
+
+
 }
