70e371e90b81d0c8af702b2388afae9667a6b1be
JeongWonYoung <nicednjsdud@naver.com>
2025-09-11 13:21:18 +0900

    대안 2 지연 조회

---
+++ SpringAop/aop/src/main/java/hello/aop/internalcall/CallServiceV2.java
@@ -0,0 +1,27 @@
+package hello.aop.internalcall;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class CallServiceV2 {
+    
+    private final ObjectProvider<CallServiceV2> callServiceProvider;
+
+    public CallServiceV2(@Lazy ObjectProvider<CallServiceV2> callServiceProvider) {
+        this.callServiceProvider = callServiceProvider;
+    }
+
+    public void external() {
+        log.info("call external");
+        CallServiceV2 callServiceV1 = callServiceProvider.getObject();
+        callServiceV1.internal(); //this.internal()
+    }
+
+    public void internal() {
+        log.info("call internal");
+    }
+}
